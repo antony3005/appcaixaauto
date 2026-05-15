5c29db29a074d7945ef9ca74c466f68a60526f7c
Antony <telesantony51@gmail.com>
2026-05-15 00:22:22 -0300

    Finalização do Item do carrinho - Faltando a lista para implementar na tela do carrinho de compra

--- lib/src/Widget/car_item.dart
+++ lib/src/Widget/car_item.dart
@@ -1,12 +1,49 @@
 import 'package:flutter/material.dart';
 
-class CarItem extends StatelessWidget {
-  const CarItem({super.key});
+  // ignore: non_constant_identifier_names
+  Widget CarItem(String nome, String preco, String imagemUrl) {
+    return Card(
+      margin: const EdgeInsets.symmetric(horizontal: 15, vertical: 6),
+      child: Padding(
+        padding: const EdgeInsets.all(8), 
+        child: Row(
+          children: [
+            Container(
+              width: 80,
+              height: 80,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                image: DecorationImage(
+                  image: NetworkImage(""),
+                  fit: BoxFit.cover,
+                  )
+              ),
+            ),
+            const SizedBox( width: 15),
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text("nome", style: const TextStyle(
+                    fontSize: 15, fontWeight: FontWeight.bold
+                    ),
+                    ),
+                    const SizedBox(height: 5,),
+                    Text("R\$ preço", style: const TextStyle(color: Colors.green),)
+                ],
+              ),
+            ),
+            Row(
+              children: [
+                IconButton(onPressed: (){}, icon: const Icon(Icons.remove_circle_outline)),
+                Text("1"),
+                IconButton(onPressed: () {}, icon: const Icon(Icons.add_circle_outline, color: Colors.blue)),
+              ],
+              )
+          ],  
+        )
+        ),
+
     );
   }
-}
